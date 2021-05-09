990d74270cf15b718a9ffec6bda07ac3460d66c2
ChechuT10 <jesusito1010@gmail.com>
2021-05-09 18:38:13 +0200

    conflictos solucionados

--- app/informes/informes.php
+++ app/informes/informes.php
@@ -1,15 +1,4 @@
 <?php require_once '../templates/header.php' ?>
-<div class="content">
-    <canvas id="myCanvas"></canvas>
-    <legend for="myCanvas"></legend>
-</div>
-<div class="back-image">
-    <?php require_once '../templates/footer.php' ?>
-    <script type="text/javascript" src="../js/informes.js"></script>
-</div>
-
-
-<?php require_once '../templates/header.php' ?>
 
 <div class="content">
     <canvas id="myCanvas"></canvas>
--- app/templates/header.php
+++ app/templates/header.php
@@ -10,18 +10,10 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
     <link rel="stylesheet" type="text/css" href="../css/alimentos.css" />
     <link rel="stylesheet" type="text/css" href="../css/ejercicio.css" />
     <link rel="stylesheet" type="text/css" href="../css/forms.css" />
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
-=======
-    <link rel="stylesheet" type="text/css" href="../css/alimentos.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/ejercicio.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/forms.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/style.css"/>
-    <link rel="stylesheet" href="../css/informes.css">
->>>>>>> rama_JT
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
