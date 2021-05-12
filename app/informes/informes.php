23dcd88a278378f824294b0ee0ba777b898b9dd5
ChechuT10 <jesusito1010@gmail.com>
2021-05-12 12:40:47 +0200

    informes arreglado (falta css)

--- app/informes/informes.php
+++ app/informes/informes.php
@@ -1,12 +1,16 @@
 <?php require_once '../templates/header.php' ?>
 
 <div class="content">
+    <form id="envia" method="post">
+        <input type="date" name="fecha" id="fecha">
+    </form>
     <canvas id="myCanvas"></canvas>
     <legend class="lege" for="myCanvas"></legend>
 
 </div>
 
 <?php require_once '../templates/footer.php' ?>
+<script src="../js/calendario.js"></script>
 <script type="text/javascript" src="../js/informes.js"></script>
 
 </html>
