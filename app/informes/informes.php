1d475de3ab6044ce4005635df105d1a4a1b35280
tipan <carlos.tipan.rodr@gmail.com>
2021-05-13 19:45:30 +0200

    añadido fecha en informes

--- app/informes/informes.php
+++ app/informes/informes.php
@@ -1,4 +1,20 @@
 <?php require_once '../templates/header.php' ?>
+<?php
+    require_once '../class/dbc.php';
+    require_once '../class/food.php';
+
+    if(isset($_GET['date'])){
+        $_SESSION['date'] = $_GET['date'];
+    }else{
+        $auxdate = getdate();
+        $year = $auxdate['year'];
+        $month = $auxdate['mon'];
+        $day = $auxdate['mday'];
+        $date = "$year-$month-$day";
+        $_SESSION['date'] = $date;
+    }
+?>
+
 <div class="content">
     <form id="envia" method="post">
         <input type="date" name="fecha" id="fecha">
