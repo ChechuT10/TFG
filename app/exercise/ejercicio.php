b1dc1a215eac19035fbc660cd7bd4986645d7395
tipan <carlos.tipan.rodr@gmail.com>
2021-05-13 09:21:18 +0200

    merge

--- app/exercise/ejercicio.php
+++ app/exercise/ejercicio.php
@@ -1,6 +1,7 @@
 <?php require_once '../templates/header.php' ?>
 <div class="add-image-ejer queryhide">
     <div class="content">
+<<<<<<< HEAD
         <div class="registro-diario">
             <h4>Tu registro de ejercicios para: <h4>
             <form id="envia" method="post">
@@ -25,9 +26,26 @@
                     <p>Añadir</p>
                 </div>
             </div>
+=======
+        <form id="envia" method="post">
+            <input type="date" name="fecha" id="fecha">
+        </form>
+        <div class="ejercicios">
+            <div class="cardio">
+                <h3>Cardiovascular</h3>
+                <p>Añadir</p>
+                <div class="otro">
+                    <h3>Ejercicios de fuerza</h3>
+                    <p>Añadir</p>
+                </div>
+            </div>
+        </div>
+>>>>>>> 1cad5d1 (Modificacion de la bbdd)
     </div>
-</div>   
+</div>
+</div>
 <script src="../js/calendario.js"></script>
-<?php require_once '../templates/footer.php'?>
+<?php require_once '../templates/footer.php' ?>
 <!-- <script src="../datepiker/calendar.js"></script> -->
+
 </html>
